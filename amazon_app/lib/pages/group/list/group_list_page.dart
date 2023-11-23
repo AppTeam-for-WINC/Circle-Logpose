af1b3ff0ec958a749c0f02af5c8ee7e775a15096
shotaro <okita0510sb@i.softbank.jpgit config --global user.email okita0510sb@i.softbank.jp>
2023-11-23 15:51:17 +0900

    group_list_pageを変更しました

--- amazon_app/lib/pages/group/list/group_list_page.dart
+++ amazon_app/lib/pages/group/list/group_list_page.dart
@@ -1,23 +1,267 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-//担当：　沖田昇太郎
+import '../../function/slide_segmented_tab_control.dart';
+import '../create/group_create_page.dart';
 
 class GroupListPage extends ConsumerWidget {
   const GroupListPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const CupertinoApp(
-      home: CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          //タイトル
-          middle: Text('Hello World'),
+    return CupertinoApp(
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: const Color(0xFFF5F3FE),
+          child: DefaultTabController(
+            length: 2,
+            child: SafeArea(
+              child: Stack(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 50,
+                        ),
+                        child: Container(
+                          padding: const EdgeInsets.only(left: 14, right: 14),
+                          width: 375,
+                          height: 77,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(999),
+                            boxShadow: const [
+                              BoxShadow(
+                                  blurRadius: 4,
+                                  spreadRadius: 3,
+                                  offset: Offset(0, 3),
+                                  color: Color.fromRGBO(0, 0, 0, 0.25)),
+                            ],
+                            border: Border.all(
+                              color: const Color.fromRGBO(4, 49, 57, 0.05),
+                            ),
+                          ),
+                          child: SegmentedTabControl(
+                            radius: const Radius.circular(999),
+                            backgroundColor: Colors.white,
+                            tabTextColor: Colors.black,
+                            selectedTabTextColor: Colors.white,
+                            squeezeIntensity: 2,
+                            height: 55,
+                            tabs: [
+                              SegmentTab(
+                                textColor: Colors.black,
+                                label: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      width: 33,
+                                      height: 33,
+                                      decoration: BoxDecoration(
+                                        color: const Color.fromRGBO(
+                                            255, 255, 255, 0.20),
+                                        borderRadius: BorderRadius.circular(33),
+                                      ),
+                                      child: const Center(
+                                        child: Text(
+                                          'all',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w700,
+                                            color: Color.fromRGBO(
+                                                255, 255, 255, 0.80),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    const Text(
+                                      '出席簿',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    const Icon(
+                                      Icons.expand_more,
+                                      size: 30,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                                // For example, this overrides [indicatorColor] from [SegmentedTabControl]
+                                color: const Color(0xFF7B61FF),
+                              ),
+                              SegmentTab(
+                                  label: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.group,
+                                        size: 30,
+                                        color: Colors.white,
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(
+                                          left: 5,
+                                        ),
+                                        child: const Text(
+                                          '団体',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  // backgroundColor: Color(0xFF7B61FF),
+                                  selectedTextColor: Colors.white,
+                                  textColor: Colors.black,
+                                  color: const Color(0xFF7B61FF)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // Sample pages
+                  Padding(
+                    padding: EdgeInsets.only(top: 130),
+                    child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GridView.count(
+                      primary: false,
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.all(20),
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      crossAxisCount: 2,
+                      children: <Widget>[
+                        GroupeBox(),
+                        GroupeBox(),
+                        GroupeBox(),
+                        GroupeBox(),
+                        GroupeBox(),
+                        GroupeBox(),
+                        GroupeBox(),
+                        GroupeBox(),
+                      ],
+                    ),
+                  ],
+                ),
+              
+            ),
+                    ),
+                    // child: TabBarView(
+                    //   physics: BouncingScrollPhysics(),
+                    //   children: [
+                    //     SingleChildScrollView(
+                    //       child: Column(
+                            
+                            
+                    //       ),
+                          
+                    //     ),
+                    //   ],
+                    // ),
+                ],
+              ),
+            ),
+          ),
         ),
-        child: Center(
-          child: Text('iPhone15 pro 欲しいです。誰か買って下さい。連絡待ってます'),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Container(
+          width: double.infinity,
+          height: 120,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color(0xff76548C).withOpacity(0),
+                const Color(0xff0F0439).withOpacity(0.3),
+                const Color(0xff0F0439).withOpacity(0.4),
+                const Color.fromARGB(255, 159, 146, 225).withOpacity(0.7),
+              ],
+              stops: const [0, 0.5, 0.8, 0.99],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Positioned(
+                top: 15,
+                child: SizedBox(
+                  width: 250,
+                  height: 55,
+                  child: FloatingActionButton.extended(
+                    backgroundColor: const Color(0xFF7B61FF),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => GroupCreatePage()),
+                      );
+                    },
+                    label: const Text(
+                      '新しい団体を作成',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    icon: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.add,
+                          size: 25,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+class GroupeBox extends ConsumerWidget{
+  const GroupeBox({Key?key,}) : super(key:key);
+  @override
+  Widget build (BuildContext context, WidgetRef ref){
+    return Container(
+      padding: const EdgeInsets.all(24),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(28),
+                        color: Colors.white,
+                      ),
+                      child:Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                      Icon(Icons.rocket),
+                      const Text("団体名",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),),
+                      ],),
+    );
+  }
+}
+
