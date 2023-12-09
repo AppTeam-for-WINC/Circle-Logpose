f7751216aaf5db0652c7f4c94c4f19eb13b76c99
yugo55 <akasatana0064@gmail.com>
2023-12-09 16:06:06 +0900

    schedule.dartを記述しました

---
+++ amazon_app/lib/database/riverpod.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final firestoreProvider = Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
+
+// ref.read()
+// ref.watch()
+
+// int alpha = FirebaseFirestore(
+//     x = 10;
+// );
+// alpha = 20;
+
+//https://qiita.com/taisei_dev/items/4c9d9572a56051a1d51f
+
