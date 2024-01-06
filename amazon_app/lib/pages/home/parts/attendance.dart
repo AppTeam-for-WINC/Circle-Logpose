f6a30e6845760dd0e99fa3104d0ed4ad1924a52e
akasa <akasa@MacBook-Air.local>
2024-01-06 16:55:33 +0900

    attendance.dartファイルのfloatingActionButtonをスマホの一番下に表示するように修正

--- amazon_app/lib/pages/home/parts/attendance.dart
+++ amazon_app/lib/pages/home/parts/attendance.dart
@@ -9,6 +9,7 @@ class AttendanceRecord extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final deviceH = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -39,68 +40,71 @@ class AttendanceRecord extends ConsumerWidget {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Container(
-        width: double.infinity,
-        height: 120,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              const Color(0xff76548C).withOpacity(0),
-              const Color(0xff0F0439).withOpacity(0.3),
-              const Color(0xff0F0439).withOpacity(0.4),
-              const Color.fromARGB(255, 159, 146, 225).withOpacity(0.7),
-            ],
-            stops: const [0, 0.5, 0.8, 0.99],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+      floatingActionButton: Transform.translate(
+        offset: Offset(0, deviceH * 0.036),
+        child: Container(
+          width: double.infinity,
+          height: 120,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color(0xff76548C).withOpacity(0),
+                const Color(0xff0F0439).withOpacity(0.3),
+                const Color(0xff0F0439).withOpacity(0.4),
+                const Color.fromARGB(255, 159, 146, 225).withOpacity(0.7),
+              ],
+              stops: const [0, 0.5, 0.8, 0.99],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            Positioned(
-              top: 15,
-              child: SizedBox(
-                width: 180,
-                height: 55,
-                child: FloatingActionButton.extended(
-                  backgroundColor: const Color(0xFF7B61FF),
-                  onPressed: () async{
-                    await showCupertinoModalPopup<ScheduleCreatePopup>(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return const Center(
-                          child: ScheduleCreatePopup(),
-                        );
-                      },
-                    );
-                  },
-                  label: const Text(
-                    '予定を作成',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.white,
+          child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Positioned(
+                top: 15,
+                child: SizedBox(
+                  width: 180,
+                  height: 55,
+                  child: FloatingActionButton.extended(
+                    backgroundColor: const Color(0xFF7B61FF),
+                    onPressed: () async {
+                      await showCupertinoModalPopup<ScheduleCreatePopup>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return const Center(
+                            child: ScheduleCreatePopup(),
+                          );
+                        },
+                      );
+                    },
+                    label: const Text(
+                      '予定を作成',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  icon: Container(
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.white,
-                    ),
-                    child: const Center(
-                      child: Icon(
-                        Icons.add,
-                        size: 25,
-                        color: Colors.black,
+                    icon: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.white,
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.add,
+                          size: 25,
+                          color: Colors.black,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
