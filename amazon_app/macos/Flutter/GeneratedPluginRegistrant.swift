15edf8d5d135574efa62983dfffe2bf5b0481ac3
Youngman <youngman@YoungmannoMacBook-Air.local>
2024-02-03 21:49:07 +0900

    Set localizations

--- amazon_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ amazon_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -26,7 +26,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
