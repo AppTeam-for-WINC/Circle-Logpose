8ae885706bb77311e9bac5e3e346bf0ccb477c9d
YoungmanCH <tsubasa22business@gmail.com>
2024-04-21 14:17:44 +0900

    tempo save

--- logpose/lib/src/controllers/providers/group/group/group_setting_provider.dart
+++ logpose/lib/src/controllers/providers/group/group/group_setting_provider.dart
@@ -21,23 +21,25 @@ class _GroupSettingNotifier extends StateNotifier<GroupProfile?> {
   String groupId;
 
   Future<void> _initGroupProfile() async {
-    if (groupId.isEmpty) {
-      state = null;
-    }
     try {
-      final groupData = await GroupController.watch(groupId).first;
-      if (groupData != null) {
-        state = groupData;
-        groupNameController.text = groupData.name;
-      } else {
-        state = null;
-      }
+      final groupData = await _fetchGroupProfile();
+      state = groupData;
+      groupNameController.text = groupData.name;
     } on Exception catch (e) {
       state = null;
-      throw Exception('Error: No found group $e');
+      debugPrint('Error: No found group $e');
     }
   }
 
+  Future<GroupProfile> _fetchGroupProfile() async {
+    final groupData = await GroupController.watch(groupId).first;
+    if (groupData == null) {
+      state = null;
+    }
+    return groupData!;
+  }
+
+  /// Change GroupProfile image.
   Future<void> changeImage(File newImage) async {
     if (state != null) {
       state = state!.copyWith(image: newImage.path);
